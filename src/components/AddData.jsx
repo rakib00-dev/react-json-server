886f3962f23ebd5bd594988d3dcf836464468249
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-05-19 13:56:23 +0600

    added two buttons and its color

--- src/components/AddData.jsx
+++ src/components/AddData.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const AddData = ({ data }) => {
   return (
-    <div>
+    <div className="">
       {data.map((e, i) => (
         <div
-          className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          className="grid max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 gap-5"
           key={i}
         >
           <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -16,6 +16,14 @@ const AddData = ({ data }) => {
             Here are the biggest enterprise technology acquisitions of 2021 so
             far, in reverse chronological order.
           </p>
+          <div className="w-full flex justify-around gap-2">
+            <button className="transition-all duration-300 w-full rounded-md py-2 px-2 font-bold bg-green-500 text-white cursor-pointer hover:bg-green-200 hover:text-black">
+              Edit
+            </button>
+            <button className="transition-all duration-300 w-full rounded-md py-2 px-2 font-bold bg-red-500 text-white cursor-pointer hover:bg-red-200 hover:text-black">
+              Delete
+            </button>
+          </div>
         </div>
       ))}
     </div>
--- src/components/TakeInput.jsx
+++ src/components/TakeInput.jsx
@@ -28,13 +28,13 @@ const TakeInput = () => {
         <input
           type="search"
           id="default-search"
-          class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none"
+          class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500 outline-none "
           placeholder="Search Mockups, Logos..."
           required
         />
         <button
           type="submit"
-          class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="text-white absolute end-2.5 bottom-2.5 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 cursor-pointer"
         >
           Submit
         </button>
