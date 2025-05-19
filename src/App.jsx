2146df311b8ea4de1723560fd5dddff571c991d0
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-05-19 11:43:14 +0600

    added input feild and added box styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 
+// components
+import TakeInput from './TakeInput';
+
 function App() {
   const [data, setData] = useState([]);
 
@@ -15,13 +18,24 @@ function App() {
   console.log(data);
 
   return (
-    <>
+    <div className="m-10 grid gap-5">
+      <TakeInput />
       {data.map((e, i) => (
-        <div key={i}>
-          <h1>{e.title}</h1>
+        <div
+          className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          key={i}
+        >
+          <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {e.title}
+          </h1>
+
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
         </div>
       ))}
-    </>
+    </div>
   );
 }
 
