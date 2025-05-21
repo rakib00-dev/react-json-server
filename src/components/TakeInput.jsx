6c77c30977158c37f4c6033066fe06bdf3ef522b
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-05-21 16:36:28 +0600

    updated the update funcationality cancel button

--- src/components/TakeInput.jsx
+++ src/components/TakeInput.jsx
@@ -41,69 +41,72 @@ const TakeInput = ({
   console.log(changingData);
 
   return (
-    <form className="mx-auto w-full grid gap-2" onSubmit={handleSubmit}>
-      <div className="grid md:grid-cols-2 gap-2">
-        <div>
-          <label htmlFor="title" className="text-md font-semibold">
-            Title
-          </label>
-          <input
-            ref={titleRef}
-            type="text"
-            id="title"
-            className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  outline-none "
-            placeholder="Write Blog Title"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="author" className="text-md font-semibold">
-            Author
-          </label>
-          <input
-            ref={authorRef}
-            type="text"
-            id="author"
-            className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  outline-none "
-            placeholder="Author Name"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="date" className="text-md font-semibold">
-            Date
-          </label>
-          <input
-            ref={dateRef}
-            type="text"
-            id="date"
-            className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  outline-none "
-            placeholder="YYYY-MM-DD"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="content" className="text-md font-semibold">
-            Content
-          </label>
-          <textarea
-            ref={contentRef}
-            name="content"
-            id="content"
-            className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 outline-none h-13"
-            placeholder="Write Your Blog Content"
-            required
-          ></textarea>
-        </div>
-      </div>
-      <div className="w-full flex justify-end gap-2">
-        {isEditClicked ? (
-          <>
+    <>
+      {isEditClicked ? (
+        <form
+          className="mx-auto w-full grid gap-2"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <div className="grid md:grid-cols-2 gap-2">
+            <div>
+              <label htmlFor="title" className="text-md font-semibold">
+                Title
+              </label>
+              <input
+                ref={titleRef}
+                type="text"
+                id="title"
+                className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  outline-none "
+                placeholder="Write Blog Title"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="author" className="text-md font-semibold">
+                Author
+              </label>
+              <input
+                ref={authorRef}
+                type="text"
+                id="author"
+                className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  outline-none "
+                placeholder="Author Name"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="date" className="text-md font-semibold">
+                Date
+              </label>
+              <input
+                ref={dateRef}
+                type="text"
+                id="date"
+                className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  outline-none "
+                placeholder="YYYY-MM-DD"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="content" className="text-md font-semibold">
+                Content
+              </label>
+              <textarea
+                ref={contentRef}
+                name="content"
+                id="content"
+                className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 outline-none h-13"
+                placeholder="Write Your Blog Content"
+                required
+              ></textarea>
+            </div>
+          </div>
+          <div className="w-full flex justify-end gap-2">
             <button
               className="transition-all duration-300 text-white relative bg-gray-500 hover:bg-gray-300 hover:text-black font-medium rounded-lg text-sm px-6 py-3 cursor-pointer w-fit"
               onClick={() => {
                 setIsEditClicked(false);
-                setChangingData(null);
+                window.location.reload();
               }}
             >
               Cancel
@@ -111,18 +114,76 @@ const TakeInput = ({
             <button className="transition-all duration-300 text-white relative bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-6 py-3 cursor-pointer w-fit">
               Update
             </button>
-          </>
-        ) : (
-          <button
-            type="submit"
-            className=" text-white relative bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-6 py-3 cursor-pointer w-fit"
-            disabled={isBtnClicked}
-          >
-            Submit
-          </button>
-        )}
-      </div>
-    </form>
+          </div>
+        </form>
+      ) : (
+        <form className="mx-auto w-full grid gap-2" onSubmit={handleSubmit}>
+          <div className="grid md:grid-cols-2 gap-2">
+            <div>
+              <label htmlFor="title" className="text-md font-semibold">
+                Title
+              </label>
+              <input
+                ref={titleRef}
+                type="text"
+                id="title"
+                className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  outline-none "
+                placeholder="Write Blog Title"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="author" className="text-md font-semibold">
+                Author
+              </label>
+              <input
+                ref={authorRef}
+                type="text"
+                id="author"
+                className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  outline-none "
+                placeholder="Author Name"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="date" className="text-md font-semibold">
+                Date
+              </label>
+              <input
+                ref={dateRef}
+                type="text"
+                id="date"
+                className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  outline-none "
+                placeholder="YYYY-MM-DD"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="content" className="text-md font-semibold">
+                Content
+              </label>
+              <textarea
+                ref={contentRef}
+                name="content"
+                id="content"
+                className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 outline-none h-13"
+                placeholder="Write Your Blog Content"
+                required
+              ></textarea>
+            </div>
+          </div>
+          <div className="w-full flex justify-end gap-2">
+            <button
+              type="submit"
+              className=" text-white relative bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-6 py-3 cursor-pointer w-fit"
+              disabled={isBtnClicked}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
+    </>
   );
 };
 
